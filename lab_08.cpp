8a4555b4ff57d7f57994ecaff4d3f90f954af2de
alimaqsood11 <alimaqsood5077@gmail.com>
2024-10-10 10:12:03 +0500

    commit 3

--- lab_08.cpp
+++ lab_08.cpp
@@ -4,5 +4,8 @@ int main(){
     cout<<"Hello World!";
     cout<<"How are you?";
     cout<<"I'm fine.";
+    cout<<"This is ICT-LAB_08";
+    cout<<"The tasks is GIT and GITHUB";
+
     return 0;
 }
